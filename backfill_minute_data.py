0ed6b995cd77bf3fe130f7d9ca895794d11fbe88
Stephon <srerrie@gmail.com>
2022-07-28 18:27:05 +0000

    fixed backfill script

--- backfill_minute_data.py
+++ backfill_minute_data.py
@@ -1,10 +1,11 @@
 from historicaldatahelper import CBProHistDataRetriever
 import datetime
 import pandas as pd
+import telegram_send as t
 
 end = pd.Timestamp(datetime.datetime.now())
 start = end - pd.Timedelta(weeks = 3 * 52)
-assets = ['BTC-USD', 'ETH-USD', 'LTC-USD']
+assets = ['ETH-USD', 'LTC-USD']
 
 if __name__ == '__main__':
 
@@ -12,18 +13,19 @@ if __name__ == '__main__':
     granularity = 60
 
     for asset in assets:
-        filename = f'asset_minute_data.parquet'
-        print(f"Retrieving data for {asset}.....")
+        filename = f'{asset}_minute_data.parquet'
+        t.send(messages = [f"Retrieving data for {asset}....."])
         historical_data = data_retriever.get_historical_prices(
             product=asset,
             start_date=start,
             end_date=end,
             granularity=granularity
         )
-        print("Done getting data")
         # Then we are going to save this to some parquet file
         historical_data.to_parquet(filename)
-        print("Done saving data to {}".format(filename))
+        t.send(messages = ["Done saving data to {}".format(filename)])
+
+
 
 
 
